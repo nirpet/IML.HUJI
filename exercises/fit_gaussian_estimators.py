052da050c5f0a45d098d030518158040d0933348
Nir Petruschka <nirpet111@gmail.com>
2022-03-23 14:13:48 +0200

    bug fixes

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -1,10 +1,7 @@
-import math
-
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
-import plotly.express as px
 
 pio.templates.default = "simple_white"
 
@@ -20,11 +17,6 @@ def test_univariate_gaussian():
     univariate_gaussian.fit(X)
     print('(' + str(univariate_gaussian.mu_) + ', ' + str(univariate_gaussian.var_) + ')')
 
-    test = np.array([1, 5, 2, 3, 8, -4, -2, 5, 1, 10, -10, 4, 5, 2, 7, 1, 1, 3, 2, -1, -3, 1, -4, 1, 2, 1,
-          -4, -4, 1, 3, 2, 6, -6, 8, 3, -6, 4, 1, -2, 3, 1, 4, 1, 4, -2, 3, -1, 0, 3, 5, 0, -2])
-    print(univariate_gaussian.log_likelihood(1, 1, test))
-    print(univariate_gaussian.log_likelihood(10, 1, test))
-
     # # Question 2 - Empirically showing sample mean is consistent
     sample_sizes = np.arange(10, 1010, 10)
     estimation_dist = np.empty(100)
@@ -32,20 +24,22 @@ def test_univariate_gaussian():
         estimation_dist[i] = np.abs(mean - np.mean(X[:sample_sizes[i]]))
 
     fig = go.Figure([go.Scatter(x=sample_sizes, y=estimation_dist, mode='markers+lines', marker=dict(color="black"))],
-                    layout=go.Layout(title_text=r"$\text{distance from actual mean by sample size}$",
+                    layout=go.Layout(title_text=r"$\text{Distance from actual mean by sample size}$",
                                      xaxis_title="sample size",
-                                     yaxis_title="distance from actual mean",
-                                     height=300))
+                                     yaxis_title="distance",
+                                     width=800,
+                                     height=600))
     fig.show()
 
     # # Question 3 - Plotting Empirical PDF of fitted model
     X.sort()
     pdf_values = univariate_gaussian.pdf(X)
     fig2 = go.Figure([go.Scatter(x=X, y=pdf_values, mode='markers+lines', marker=dict(color="black"))],
-                     layout=go.Layout(title_text=r"$\text{pdf values}$",
+                     layout=go.Layout(title_text=r"$\text{PDF values of ordered samples}$",
                                       xaxis_title="sample",
-                                      yaxis_title="pdf value",
-                                      height=300))
+                                      yaxis_title="PDF",
+                                      width=800,
+                                      height=600))
     fig2.show()
 
 
@@ -65,21 +59,24 @@ def test_multivariate_gaussian():
     print(multivariate_gaussian.cov_)
 
     # Question 5 - Likelihood evaluation
-
     mean_values = np.linspace(-10, 10, 200)
     log_likelihood_data = np.empty((200, 200))
     for i in range(200):
         for j in range(200):
-            mu = [mean_values[j], 0, mean_values[i], 0]
+            mu = [mean_values[i], 0, mean_values[j], 0]
             log_likelihood_data[i][j] = multivariate_gaussian.log_likelihood(mu, cov, X)
 
-    fig = px.imshow(log_likelihood_data,
-                    title="log likelihood ",
-                    labels=dict(x="f1", y="f3", color="log likelihood"),
-                    x=mean_values,
-                    y=mean_values)
-
+    fig = go.Figure([go.Heatmap(x=mean_values, y=mean_values, z=log_likelihood_data,
+                                colorbar=dict(title='log likelihood'))],
+                    layout=go.Layout(title_text=r"$\text{Log likelihood of mean values of the given sample}$",
+                                     xaxis_title="f3",
+                                     yaxis_title="f1",
+                                     width=600,
+                                     height=600))
     fig.show()
+
+    # Question 6 - Likelihood evaluation
+    # The maximum likelihood is achieved for the following values, f1=-0.050, f3=3.970
 
 
 if __name__ == '__main__':
