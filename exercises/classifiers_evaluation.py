3e11b60f08f7cad8892fb496692a0deb6476759d
Nir Petruschka <nirpet111@gmail.com>
2022-04-26 15:45:55 +0300

    created graphs

--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 from IMLearn.learners.classifiers import Perceptron, LDA, GaussianNaiveBayes
 from typing import Tuple
 from utils import *
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
+
 pio.templates.default = "simple_white"
 from math import atan2, pi
 
@@ -104,28 +107,36 @@ def compare_gaussian_classifiers():
         fig = make_subplots(rows=1, cols=2,
                             subplot_titles=["Linear discriminant analysis, Accuracy: " + str(lda_accuracy),
                                             "Gaussian naive Bayes, Accuracy: " + str(gnb_accuracy)],
-                            horizontal_spacing=0.01, vertical_spacing=.03, )
-        fig.update_layout(title="Dataset: " + f)
+                            horizontal_spacing=0.01, vertical_spacing=.03)
+        fig.update_layout(title="Dataset: " + f, showlegend=False)
+        lda_results = go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
+                                 marker=dict(color=lda_y_pred, symbol=y, size=15))
+        lda_data = [lda_results]
+        for i in range(lda.classes_.shape[0]):
+            lda_ellipse = get_ellipse(lda.mu_[i], lda.cov_)
+            lda_X = go.Scatter(x=[lda.mu_[i][0]], y=[lda.mu_[i][1]], mode="markers", showlegend=False,
+                               marker=dict(color='black', symbol='x', size=30))
+            lda_data.append(lda_ellipse)
+            lda_data.append(lda_X)
 
-        fig.add_traces(go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
-                                  marker=dict(color=lda_y_pred, symbol=y, size=15)),
-                       rows=1, cols=1)
+        fig.add_traces(lda_data, rows=1, cols=1)
 
-        fig.add_traces(go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
-                                  marker=dict(color=gnb_y_pred, symbol=y, size=15)),
-                       rows=1, cols=2)
+        gnb_results = go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
+                                 marker=dict(color=gnb_y_pred, symbol=y, size=15))
+        gnb_data = [gnb_results]
 
+        for i in range(gnb.classes_.shape[0]):
+            cov_matrix = np.zeros(shape=(2, 2))
+            cov_matrix[0][0] = gnb.vars_[i][0]
+            cov_matrix[1][1] = gnb.vars_[i][1]
+            gnb_X = go.Scatter(x=[gnb.mu_[i][0]], y=[gnb.mu_[i][1]], mode="markers", showlegend=False,
+                               marker=dict(color='black', symbol='x', size=30))
+            gnb_ellipse = get_ellipse(gnb.mu_[i], cov_matrix)
+            gnb_data.append(gnb_ellipse)
+            gnb_data.append(gnb_X)
+
+        fig.add_traces(gnb_data, rows=1, cols=2)
         fig.show()
-        # fig.add_traces([decision_surface(m.fit(X, y).predict, lims[0], lims[1], showscale=False),
-        #                 go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
-        #                            marker=dict(color=y, symbol=symbols[y], colorscale=[custom[0], custom[-1]],
-        #                                        line=dict(color="black", width=1)))],
-        #                rows=(i // 3) + 1, cols=(i % 3) + 1)
-        #
-        # fig.update_layout(title=rf"$\textbf{{(2) Decision Boundaries Of Models - {title} Dataset}}$",
-        #                   margin=dict(t=100)) \
-        #     .update_xaxes(visible=False).update_yaxes(visible=False)
-
 
 if __name__ == '__main__':
     np.random.seed(0)
